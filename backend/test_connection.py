4fbf897839a70dbdf570344cbf28571fd7b52e71
Angie Julieth Barreto Guerrero <Angie.Barreto@admin.mosquera>
2025-04-03 09:07:15 -0500

    Actualizacion

--- backend/test_connection.py
+++ backend/test_connection.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 import psycopg2
+from urllib.parse import urlparse
 
 load_dotenv()  
 
@@ -15,6 +16,22 @@ DATABASE_URL = f"{DATABASE_URL}?sslmode=require"
 try:
     engine = create_engine(DATABASE_URL)
     with engine.connect() as connection:
-        print("Conexión exitosa a PostgreSQL")
+        print("Conexión exitosa a PostgreSQL con SQLAlchemy")
 except Exception as e:
     print(f"Error al conectar con la base de datos: {e}")
+
+
+try:
+    result = urlparse(DATABASE_URL)
+    
+    connection = psycopg2.connect(
+        dbname=result.path[1:],  
+        user=result.username,
+        password=result.password,
+        host=result.hostname,
+        port=result.port
+    )
+    print("Conexión exitosa a PostgreSQL con psycopg2")
+    connection.close()
+except Exception as e:
+    print(f"Error al conectar con psycopg2: {e}")
