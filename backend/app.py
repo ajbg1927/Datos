7d6c130cb5e598b9621a16a98764b02c97cd1541
Angie Julieth Barreto Guerrero <Angie.Barreto@admin.mosquera>
2025-04-04 12:12:49 -0500

    Actualizacion

--- backend/app.py
+++ backend/app.py
@@ -16,7 +16,7 @@ load_dotenv()
 
 app = Flask(__name__)
 app.config.from_object(Config)
-CORS(app, resources={r"/*": {"origins": "http://localhost:3000", "https://datosfrontend.vercel.app/"}})
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 UPLOAD_FOLDER = "uploads"
 ALLOWED_EXTENSIONS = {"xls", "xlsx"}
