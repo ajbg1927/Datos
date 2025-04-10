6112aa5665ffba8b7f97e79597136e7e62c6d656
Angie Barreto <164963166+ajbg14@users.noreply.github.com>
2025-04-10 16:42:10 -0500

    Actualización

--- backend/database/models.py
+++ backend/database/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from sqlalchemy.orm import relationship
-from backend.database.base import Base  
+from .base import Base
 
 class Archivo(Base):
     __tablename__ = 'archivos'
