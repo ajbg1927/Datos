7b96568495b2b9efd140885c69ff01513fcfd8f6
Angie Barreto <164963166+ajbg14@users.noreply.github.com>
2025-04-14 21:42:17 -0500

    Actualización

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography, Box } from '@mui/material';
-import logo from '../../public/logo_am.png'; // Asegúrate que el path esté bien
 
 function Header() {
   return (
     <AppBar position="static" sx={{ backgroundColor: '#fff', color: '#000', boxShadow: 1 }}>
       <Toolbar sx={{ justifyContent: 'center', gap: 2, position: 'relative' }}>
-        {/* Logo izquierda */}
+        
         <Box
           component="img"
           src="/logo_am.png"
-          alt="Logo"
+          alt="Logo Municipio"
           sx={{
             height: 60,
             position: 'absolute',
             left: 16,
           }}
         />
-        {/* Título centrado */}
+    
         <Typography variant="h6" component="div" align="center" sx={{ fontWeight: 600 }}>
           Análisis de Datos – Municipio de Mosquera
         </Typography>
