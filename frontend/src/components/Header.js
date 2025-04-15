01f0c0e33c23de300809c2c21fa1604d70a1df8c
Angie Barreto <164963166+ajbg14@users.noreply.github.com>
2025-04-15 12:30:01 -0500

    Actualización

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,7 +5,7 @@ function Header() {
   return (
     <AppBar position="static" sx={{ backgroundColor: '#fff', color: '#000', boxShadow: 1 }}>
       <Toolbar sx={{ justifyContent: 'center', gap: 2, position: 'relative' }}>
-        
+
         <Box
           component="img"
           src="/logo_am.png"
@@ -16,13 +16,26 @@ function Header() {
             left: 16,
           }}
         />
-    
+
         <Typography variant="h6" component="div" align="center" sx={{ fontWeight: 600 }}>
           Análisis de Datos – Municipio de Mosquera
         </Typography>
+
+        <Box
+          component="img"
+          src="/logo_tic.png"
+          alt="Logo TIC Mosquera"
+          sx={{
+            height: 50,
+            position: 'absolute',
+            right: 16,
+          }}
+        />
+
       </Toolbar>
     </AppBar>
   );
 }
 
 export default Header;
+
