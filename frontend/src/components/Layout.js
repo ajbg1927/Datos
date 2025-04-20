9857860edc7158d4e0a3acfce534f982eb6e8a55
Angie Barreto <164963166+ajbg14@users.noreply.github.com>
2025-04-20 14:23:54 -0500

    Actualización

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -14,7 +14,7 @@ const Layout = ({ children, sidebar }) => {
       <Header />
 
       <Container maxWidth="xl" sx={{ flexGrow: 1, py: 3 }}>
-        <Grid container spacing={3}>
+        <Grid container spacing={3} sx={{ height: '100%' }}> 
           <Grid item xs={12} md={3}>
             <Paper
               elevation={1}
@@ -28,18 +28,23 @@ const Layout = ({ children, sidebar }) => {
             </Paper>
           </Grid>
 
-          <Grid item xs={12} md={9}>
+          <Grid item xs={12} md={9} sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}> 
             <Paper
               elevation={1}
               sx={{
                 p: 3,
-                height: '100%',
                 borderRadius: 3,
                 backgroundColor: 'white',
+                flexGrow: 1, 
+                display: 'flex',
+                flexDirection: 'column',
               }}
             >
-              <Box sx={{ maxHeight: 'calc(100vh - 200px)', overflowY: 'auto' }}>
-                {children}
+              <Box sx={{ overflowY: 'auto', flexGrow: 1, display: 'flex', flexDirection: 'column' }}>
+                <Box sx={{ flexGrow: 1 }}>{children}</Box> {/* El contenido principal crece */}
+                <Box sx={{ mt: 2 }}>{/* Contenedor para el botón de exportar con un margen superior */}
+                  {React.Children.toArray(children).find(child => child?.type?.name === 'ExportButtons')}
+                </Box>
               </Box>
             </Paper>
           </Grid>
