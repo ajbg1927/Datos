3fabfd898762e084f6188c07cb0bd6cb3a445ac9
Angie Julieth Barreto Guerrero <Angie.Barreto@admin.mosquera>
2025-04-25 11:01:50 -0500

    Actualización

--- frontend/src/components/TablaDatos.js
+++ frontend/src/components/TablaDatos.js
@@ -50,6 +50,9 @@ const TablaDatos = ({ datos, columnas }) => {
         );
     }
 
+    console.log("Recibo en TablaDatos:", datos);
+    console.log("Columnas:", columnas);
+
     return (
         <TableContainer component={Paper} sx={{ maxWidth: '100%', overflowX: 'auto' }}>
             <Table size="small" stickyHeader>
