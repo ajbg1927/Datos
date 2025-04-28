fc51b43c03eec50319e0872adb34a263e33abdb6
Angie Barreto <164963166+ajbg14@users.noreply.github.com>
2025-04-28 16:30:07 -0500

    Actualización

--- frontend/src/components/ExportButtons.js
+++ frontend/src/components/ExportButtons.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button, Menu, MenuItem } from '@mui/material';
 import { FileDownload as FileDownloadIcon } from '@mui/icons-material';
-import useExportaciones from './useExportaciones'; 
+import useExportaciones from '../hooks/useExportaciones';
 
 const ExportButtons = ({ datos = [], columnas = [], filename = 'datos' }) => {
   const [anchorEl, setAnchorEl] = React.useState(null);
