302a67d5465052397eeb733f61199937af3df3eb
Angie Barreto <164963166+ajbg14@users.noreply.github.com>
2025-04-28 17:15:45 -0500

    Actualización

--- frontend/src/components/ExportFloatingButton.js
+++ frontend/src/components/ExportFloatingButton.js
@@ -1,5 +1,6 @@
+import React from 'react';
 import { Box } from '@mui/material';
-import ExportButtons from './ExportButtons'; 
+import ExportButtons from './ExportButtons';
 
 const ExportFloatingButton = ({ onExport }) => {
   return (
