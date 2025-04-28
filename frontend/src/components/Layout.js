ec9495f26bdcf4252964be260cfdb6ca8db13402
Angie Barreto <164963166+ajbg14@users.noreply.github.com>
2025-04-28 17:36:18 -0500

    Actualización

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -14,35 +14,41 @@ const Layout = ({ children, sidebar }) => {
       <Header />
 
       <Container maxWidth="xl" sx={{ flexGrow: 1, py: 3 }}>
-        <Grid container spacing={3} sx={{ height: '100%' }}> 
-          <Grid item xs={12} md={3}>
-            <Paper
-              elevation={1}
-              sx={{
-                p: 3,
-                borderRadius: 3,
-                backgroundColor: 'white',
-              }}
-            >
-              {sidebar}
-            </Paper>
-          </Grid>
+        <Grid container spacing={3} sx={{ height: '100%' }}>
+          {sidebar && (
+            <Grid item xs={12} md={3}>
+              <Paper
+                elevation={1}
+                sx={{
+                  p: 3,
+                  borderRadius: 3,
+                  backgroundColor: 'white',
+                  height: '100%',
+                }}
+              >
+                {sidebar}
+              </Paper>
+            </Grid>
+          )}
 
-          <Grid item xs={12} md={9} sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}> 
+          <Grid item xs={12} md={sidebar ? 9 : 12} sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
             <Paper
               elevation={1}
               sx={{
                 p: 3,
                 borderRadius: 3,
                 backgroundColor: 'white',
-                flexGrow: 1, 
+                flexGrow: 1,
                 display: 'flex',
                 flexDirection: 'column',
               }}
             >
               <Box sx={{ overflowY: 'auto', flexGrow: 1, display: 'flex', flexDirection: 'column' }}>
-                <Box sx={{ flexGrow: 1 }}>{children}</Box> {/* El contenido principal crece */}
-                <Box sx={{ mt: 2 }}>{/* Contenedor para el botón de exportar con un margen superior */}
+                <Box sx={{ flexGrow: 1 }}>
+                  {children}
+                </Box>
+
+                <Box sx={{ mt: 2 }}>
                   {React.Children.toArray(children).find(child => child?.type?.name === 'ExportButtons')}
                 </Box>
               </Box>
