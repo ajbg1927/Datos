6e3afcb51765dab64a31ccbd30f216d1f80cd30d
Angie Barreto <164963166+ajbg14@users.noreply.github.com>
2025-04-28 21:26:05 -0500

    Actualización

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,6 @@ import {
 } from '@mui/material';
 import { Toaster, toast } from 'react-hot-toast';
 import SearchIcon from '@mui/icons-material/Search';
-import ToggleButton from '@mui/material/ToggleButton';
-import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 
 import Layout from './components/Layout';
 import UploadFile from './components/UploadFile';
